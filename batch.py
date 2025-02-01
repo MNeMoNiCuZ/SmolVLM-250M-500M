a32d41bf66fd02c989ce78e401d7188cb461f842
MNeMoNiCuZ <60541708+MNeMoNiCuZ@users.noreply.github.com>
2025-02-01 20:39:43 +0100

    Update batch.py

--- batch.py
+++ batch.py
@@ -1,16 +1,18 @@
 import os
 import torch
+import math
 from PIL import Image
 from transformers import AutoProcessor, AutoModelForVision2Seq
 from tqdm import tqdm
 
 # User Configuration
+MODEL_SIZE = "500M"  # Options: "256M" or "500M"
 OVERWRITE_FILES = True  # Set to False to skip existing files
 PREFIX = ""  # Text to prepend to the output
 SUFFIX = ""  # Text to append to the output
-REMOVE_PREAMBLE = True  # Set to True to remove user/assistant preamble
 OUTPUT_FORMAT = ".txt"  # Set desired output format, e.g., ".txt", ".md"
-BATCH_SIZE = 10  # Number of images to process at the same time. 8 is good for a 24gb 3090
+BATCH_SIZE = 10  # Number of images to process at the same time. 10 is good for a 24gb 3090
+REMOVE_PREAMBLE = True  # Set to True to remove user/assistant preamble
 
 CUSTOM_PROMPT = "Describe this image in detail."
 
@@ -23,9 +25,10 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {DEVICE} {torch.cuda.get_device_name(0) if DEVICE == 'cuda' else ''}")
 
 # Initialize processor and model
-processor = AutoProcessor.from_pretrained("HuggingFaceTB/SmolVLM-500M-Instruct")
+model_path = f"HuggingFaceTB/SmolVLM-{MODEL_SIZE}-Instruct"
+processor = AutoProcessor.from_pretrained(model_path)
 model = AutoModelForVision2Seq.from_pretrained(
-    "HuggingFaceTB/SmolVLM-500M-Instruct",
+    model_path,
     torch_dtype=torch.bfloat16 if DEVICE == "cuda" else torch.float32,
     _attn_implementation="eager",
 )
@@ -46,8 +49,10 @@ for root, _, files in os.walk(input_folder):
             if OVERWRITE_FILES or not os.path.exists(output_path):
                 valid_images.append((image_path, output_path))
 
-# Display the number of valid images
+print(f"\nUsing model: {model_path}")
 print(f"Total valid images to process: {len(valid_images)}")
+batch_count = math.ceil(len(valid_images)/BATCH_SIZE)
+print(f"About to process {batch_count} {'batch' if batch_count == 1 else 'batches'}\n")
 
 # Batch processing with progress bar
 for i in tqdm(range(0, len(valid_images), BATCH_SIZE), desc="Processing Batches"):
